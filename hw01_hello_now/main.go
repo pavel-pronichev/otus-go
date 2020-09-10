0fde88a4e75ca896c130d95d858d9d035362bf74
Павел Проничев <mr.pavel.pronichev@gmail.com>
2020-09-10 22:16:52 +0300

    HW1 completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	t, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	now := time.Now()
+
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	fmt.Printf(
+		`current time: %s
+exact time: %s
+`, now, t.Round(0))
+
 }
